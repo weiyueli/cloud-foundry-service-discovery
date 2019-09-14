e6f7b51bb0368a3bcf0beb39c05b6ee42a744ea8
weiyueli <weiyue.li.mail@gmail.com>
2019-09-14 15:54:48 -0400

    export resolveAppId function

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 
 **Axios based cloud foundry service discovery client for node.js.**
 
-**Enable nodejs application dynamically discover and call other regiesterd apps through cloud foundry service registry**
-
-The API of cloud-foundry-service-discovery is identical to the API of axios.request, giving you familiar experience.
+**Enable nodejs application dynamically discover and resolve url of other regiesterd apps through cloud foundry service registry**
 
 ## Installing
 
@@ -17,17 +15,10 @@ npm install axios cloud-foundry-service-discovery
 ## Example
 
 ```js
-import Disvoery from 'cloud-foundry-service-discovery'
+import {resolveAppId} from 'cloud-foundry-service-discovery'
 
-Discovery.request({
-    // simply send your request to http://${app-name}/path
-    url: 'http://application/greeting'
-})
-.then(response=>console.log(response))
-.catch(error=>console.log(error))
+resolveAppId("appId").then(response=>console.log(appInstanceUrl)).catch(error=>console.log(error))
 ```
 
 ## Reference
 [Pivotal Cloud Foundry Service Registry](https://docs.pivotal.io/spring-cloud-services/2-0/common/service-registry/index.html)
-
-[NPM Axios](https://www.npmjs.com/package/axios#axiosrequestconfig-1)
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,107 @@
-import Axios, { AxiosRequestConfig, AxiosResponse } from "axios";
-import * as ServiceDiscover from "./service-discover";
+import Axios from "axios";
+import Debug from "debug";
+import * as NodeCache from "node-cache";
+import * as QueryString from "querystring";
 
-class Discovery {
+const debug = Debug("cloud-foundry-service-discovery");
+const cache = new NodeCache({ stdTTL: 30 });
 
-    public static request<T = any>(config: AxiosRequestConfig): Promise<AxiosResponse<T>> {
-        return ServiceDiscover.discover(config).then(Axios.request);
-    }
+let credentials: any;
+
+export function resolveAppId(appId: string): Promise<string> {
+
+    return setCredentials().then(() =>
+        getCache(appId)
+            .then((cachedInstances) => selectRandom(cachedInstances))
+            .catch((noCache) => getInstances(appId)
+                .then((instances) => setCache(appId, instances).catch((reject) => reject))
+                .then((instances) => selectRandom(instances))));
 }
 
-export default Discovery;
-export { Discovery };
+function getCache(key: string): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+        cache.get(key, (err, value) => {
+            if (err || !value) {
+                reject(new Error("No cache found"));
+                return;
+            }
+            debug("Found cache for %s", key);
+            resolve(value as string[]);
+        });
+    });
+}
+
+function setCache(key: string, value: string[]): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+        cache.set(key, value, (err, success) => {
+            if (err || !success) {
+                debug("Error set cache");
+                reject(value);
+                return;
+            }
+            resolve(value);
+        });
+    });
+}
+
+function selectRandom(instances: string[]): Promise<string> {
+    return new Promise((resolve, reject) => {
+        if (instances.length === 0) {
+            reject(new Error("No healthy instances available"));
+            return;
+        }
+        const randomIndex = Math.floor(Math.random() * Math.floor(instances.length));
+        resolve(instances[randomIndex]);
+    });
+}
+
+function setCredentials(): Promise<void> {
+    return new Promise((resolve, reject) => {
+        const vcapServices: string = process.env.VCAP_SERVICES || "";
+        if (!vcapServices) {
+            reject(new Error("Cloud Foundry enviroment variable VCAP_SERVICES is undefined"));
+            return;
+        }
+        const pServiceRegistry = JSON.parse(vcapServices)["p-service-registry"];
+        if (!pServiceRegistry) {
+            reject(new Error("Cloud Foundry Service [Service Registry] is not binded to application"));
+            return;
+        }
+        credentials = JSON.parse(vcapServices)["p-service-registry"][0].credentials;
+        resolve();
+    });
+}
+
+function getAccessToken(): Promise<string> {
+    return Axios.request({
+        method: "POST",
+        url: credentials.access_token_uri,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        data: QueryString.stringify({
+            client_id: credentials.client_id,
+            client_secret: credentials.client_secret,
+            grant_type: "client_credentials",
+        }),
+    }).then((response) => response.data.access_token)
+        .catch((error) => new Error("Error in getting eureka server access token"));
+}
+
+function getInstances(hostname: string): Promise<string[]> {
+    debug("Resolving hostname %s", hostname);
+    return getAccessToken()
+        .then((accessToken) => Axios.request({
+            url: credentials.uri + "/eureka/apps/" + hostname,
+            headers: {
+                Authorization: "Bearer " + accessToken,
+            },
+            responseType: "document",
+        }).then((response) => {
+            const instances = response.data.application.instance
+                .filter((i: any) => i.status === "UP")
+                .map((i: any) => i.homePageUrl);
+            debug("Hostname %s resolved to %j", hostname, instances);
+            return instances;
+        }));
+}
